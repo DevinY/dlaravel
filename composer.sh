95b6573bad7bdd1b8abaac706c7720a5d839ef5c
Devin Yang <devin@ccc.tc>
2018-05-30 00:56:53 +0800

    修正composer.sh多參數問題

--- composer.sh
+++ composer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #在Project的目錄下執行composer
-docker-compose exec -u dlaravel php bash -c "cd ${PWD##*/};composer $1"
+command="cd ${PWD##*/};composer"
+while test $# -gt 0
+do
+command="${command} ${1}"
+    shift
+done
+docker-compose exec -u dlaravel php bash -c "${command}"
